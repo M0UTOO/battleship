589971038a3a963e311e2e46206eb59a9b740ef8
Nino <ninoplane@gmail.com>
2023-04-01 02:34:55 +0200

    boat creation and game ready to play

--- main/main.go
+++ main/main.go
@@ -25,8 +25,8 @@ func main() {
 			break
 		}
 	}
-	createBoats()
-	player := player.Player{name, port}
+	listBoats := createBoats()
+	player := player.Player{name, port, listBoats}
 
 	go startServer(port, &player)
 
@@ -77,7 +77,13 @@ func sendRequest(url string, method string) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(player)
+	for _, boat := range player.Boats {
+		fmt.Println(boat.Name)
+		fmt.Println(boat.Size)
+		fmt.Println(boat.Direction)
+		fmt.Println(boat.StartingCoordinates)
+		fmt.Println(boat.BoatParts)
+	}
 }
 
 func startServer(port int, player *player.Player) {
@@ -92,48 +98,97 @@ func createBoats() []player.Boat {
 	s := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(s)
 	var boats []player.Boat
+	var isOccupied []player.Coordinates
 
 	// Carrier
-	var carrier player.Boat
-	carrier.Name = "Carrier"
-	carrier.Size = 5
-	carrier.Direction = r.Intn(4)
-	carrier.StartingCoordinates.X, carrier.StartingCoordinates.Y = getposition(carrier.Direction, carrier.Size)
-	if carrier.Direction == 0 {
-		carrier.StartingCoordinates.X = r.Intn(10) + 1
-		carrier.StartingCoordinates.Y = r.Intn(6) + 5
-	} else if carrier.Direction == 1 {
-		carrier.StartingCoordinates.X = r.Intn(6) + 1
-		carrier.StartingCoordinates.Y = r.Intn(10) + 1
-	} else if carrier.Direction == 2 {
-		carrier.StartingCoordinates.X = r.Intn(10) + 1
-		carrier.StartingCoordinates.Y = r.Intn(6) + 1
-	} else {
-		carrier.StartingCoordinates.X = r.Intn(6) + 5
-		carrier.StartingCoordinates.Y = r.Intn(10) + 1
-	}
-	carrier.BoatParts = make([]int, carrier.Size)
-	for i := 0; i < carrier.Size; i++ {
-		carrier.BoatParts[i] = 0
-	}
+	carrier := createBoat("Carrier", 5, r, &isOccupied)
 	boats = append(boats, carrier)
 
+	// Battleship
+	battleship := createBoat("Battleship", 4, r, &isOccupied)
+	boats = append(boats, battleship)
+
+	// Cruiser
+	cruiser := createBoat("Cruiser", 3, r, &isOccupied)
+	boats = append(boats, cruiser)
+
+	// Submarine
+	submarine := createBoat("Submarine", 3, r, &isOccupied)
+	boats = append(boats, submarine)
+
+	// Destroyer
+	destroyer := createBoat("Destroyer", 2, r, &isOccupied)
+	boats = append(boats, destroyer)
+
+	return boats
 }
 
-func getposition(direction, size int) (int, int) {
+func createBoat(name string, size int, r *rand.Rand, isOccupied *[]player.Coordinates) player.Boat {
+	var boat player.Boat
+	boat.Name = name
+	boat.Size = size
+	boat.Direction = r.Intn(4)
+	boat.StartingCoordinates.X, boat.StartingCoordinates.Y = getposition(boat.Direction, boat.Size, isOccupied)
+	boat.BoatParts = make([]int, boat.Size)
+	for i := 0; i < boat.Size; i++ {
+		boat.BoatParts[i] = 0
+	}
+	return boat
+}
+
+func getposition(direction, size int, isOccupied *[]player.Coordinates) (int, int) {
 	s := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(s)
-	// max := 10 - size
+	var x int
+	var y int
+	var coord player.Coordinates
+	var isGoodPositon bool
+	max := 0
 	if size == 5 {
-
+		max = 6
+	} else if size == 4 {
+		max = 7
+	} else if size == 3 {
+		max = 8
+	} else {
+		max = 9
+	}
+	for !isGoodPositon {
 		if direction == 0 {
-			return r.Intn(10) + 1, r.Intn(6) + 5
+			x = r.Intn(10) + 1
+			y = r.Intn(max) + 1
+			coord.X = x
+			coord.Y = y
+			isGoodPositon = isAlreadyOccupied(coord, isOccupied)
 		} else if direction == 1 {
-			return r.Intn(6) + 1, r.Intn(10) + 1
+			x = r.Intn(max) + size
+			y = r.Intn(10) + 1
+			coord.X = x
+			coord.Y = y
+			isGoodPositon = isAlreadyOccupied(coord, isOccupied)
 		} else if direction == 2 {
-			return r.Intn(10) + 1, r.Intn(6) + 1
+			x = r.Intn(10) + 1
+			y = r.Intn(max) + size
+			coord.X = x
+			coord.Y = y
+			isGoodPositon = isAlreadyOccupied(coord, isOccupied)
 		} else {
-			return r.Intn(6) + 5, r.Intn(10) + 1
+			x = r.Intn(max) + 1
+			y = r.Intn(10) + 1
+			coord.X = x
+			coord.Y = y
+			isGoodPositon = isAlreadyOccupied(coord, isOccupied)
 		}
 	}
+	return x, y
+}
+
+func isAlreadyOccupied(coord player.Coordinates, isOccupied *[]player.Coordinates) bool {
+	for _, c := range *isOccupied {
+		if c.X == coord.X && c.Y == coord.Y {
+			return false
+		}
+	}
+	*isOccupied = append(*isOccupied, coord)
+	return true
 }
